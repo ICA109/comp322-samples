cf7108182e0c52124184d89aa868e38052f929cd
ICA109 <t.yin1@outlook.com>
2018-02-19 15:52:33 -0500

    Added c++11 standards approach to pointers

--- L5HelloWorldPointer.cpp
+++ L5HelloWorldPointer.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
+    //int *tmp = {1, 2, 3};             //"Too many initializer variables"; 2nd half isn't an array yet
+    //int *tmp = new int[3] {1, 2, 3};  //Correct
+
+    string *sen = new string[3]{"Hello", "world", string(2, '+')};  //Requires g++ -std=c++11 mode
     
+    for (int i = 0; i<3; i++) {
+        cout << *(sen+i) << " ";
+    }
 
-
+    cout << endl;
+    delete [] sen;
+    return 0;
 }
